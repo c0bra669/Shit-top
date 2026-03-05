56eedcf1b84467ea4ad88d7901194d78114081c4
c0bra <203257071+c0bra669@users.noreply.github.com>
2026-03-05 08:01:55 +0100

    asd

--- main.cpp
+++ main.cpp
@@ -19,6 +19,58 @@ public:
     u_int64_t steal;
     u_int64_t guest;
     u_int64_t guest_nice;
+public:
+    float calc_usage(const Cpu & prev_cpu){
+        u_int64_t cur_idle=
+            this -> idle +
+            this -> iowait;
+        u_int64_t cur_busy=
+            this -> user +
+            this -> nice +
+            this -> system +
+            this -> irq +
+            this -> softirq +
+            this -> steal +
+            this -> guest +
+            this ->guest_nice;
+        u_int64_t prev_busy=
+            prev_cpu.user+
+            prev_cpu.nice+
+            prev_cpu.system+
+            prev_cpu.irq+
+            prev_cpu.softirq+
+            prev_cpu.steal+
+            prev_cpu.guest+
+            prev_cpu.guest_nice;
+        u_int64_t prev_idle=
+            prev_cpu.idle+
+            prev_cpu.iowait;
+        if(cur_idle < prev_idle || cur_busy < prev_busy){
+            return 0.0f;
+        }
+        u_int64_t idle=cur_idle-prev_idle;
+        u_int64_t busy=cur_busy-prev_busy;
+        u_int64_t total=idle+busy;
+        if (total==0){
+            return 0.0f;
+        }
+        return 100.0f * static_cast<float>(busy) / total;
+    }
+    void data_from_string(std::string &line){
+        std::istringstream iss(line);
+        iss >> this->id
+            >> this->user
+            >> this->nice
+            >> this->system
+            >> this->idle
+            >> this->iowait
+            >> this->irq
+            >> this->softirq
+            >> this->steal
+            >> this->guest
+            >> this->guest_nice;
+    }
+
 };
 class Mem{
 public:
@@ -83,44 +135,6 @@ void draw_bar(float prec){
           << "]";
 
 }
-float usage(const Cpu& curr_cpu,const Cpu& prev_cpu){
-    unsigned long  curr_idle = 
-    curr_cpu.idle+
-    curr_cpu.iowait;
-
-    unsigned long curr_busy = 
-    curr_cpu.user+
-    curr_cpu.nice+
-    curr_cpu.system+
-    curr_cpu.irq+
-    curr_cpu.softirq+
-    curr_cpu.steal+
-    curr_cpu.guest+
-    curr_cpu.guest_nice;
-
-    unsigned long prev_idle = 
-    prev_cpu.idle+
-    prev_cpu.iowait;
-
-    unsigned long prev_busy = 
-    prev_cpu.user+
-    prev_cpu.nice+
-    prev_cpu.system+
-    prev_cpu.irq+
-    prev_cpu.softirq+
-    prev_cpu.steal+
-    prev_cpu.guest+
-    prev_cpu.guest_nice;
-    
-   unsigned long idle = curr_idle-prev_idle;
-   unsigned long busy = curr_busy-prev_busy;
-   unsigned long total = idle+busy;
-   if (total == 0){
-        return 0.0f;
-   }
-    return 100.0f * static_cast<float>(busy) / total;  
-}
-
 std::vector<Cpu>read_proc(){
     std::vector<Cpu>data;
     std::ifstream file("/proc/stat");
@@ -131,20 +145,20 @@ std::vector<Cpu>read_proc(){
         if (line.rfind("cpu", 0) != 0)
             break;
 
-        std::istringstream iss(line);
+        //std::istringstream iss(line);
         Cpu cpu;
-
-        iss >> cpu.id
-            >> cpu.user
-            >> cpu.nice
-            >> cpu.system
-            >> cpu.idle
-            >> cpu.iowait
-            >> cpu.irq
-            >> cpu.softirq
-            >> cpu.steal
-            >> cpu.guest
-            >> cpu.guest_nice;
+        cpu.data_from_string(line);
+        // iss >> cpu.id
+        //     >> cpu.user
+        //     >> cpu.nice
+        //     >> cpu.system
+        //     >> cpu.idle
+        //     >> cpu.iowait
+        //     >> cpu.irq
+        //     >> cpu.softirq
+        //     >> cpu.steal
+        //     >> cpu.guest
+        //     >> cpu.guest_nice;
 
        data.push_back(cpu);
     }
@@ -164,7 +178,7 @@ int main(int argc,char **argv){
      prev_cpus=curr_cpus;
      curr_cpus=read_proc();
      for (int i = 0; i < curr_cpus.size(); ++i) {
-        float u = usage(curr_cpus[i], prev_cpus[i]);
+        float u = curr_cpus[i].calc_usage(prev_cpus[i]);
         std::cout<< curr_cpus[i].id;
         draw_bar(u);
         std::cout<<u<<"%\n";
